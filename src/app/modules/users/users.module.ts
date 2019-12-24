58f3699799931e42f697b92db610a38c69f72a98
vovich_l <lyubchenko.vladimir@gmail.com>
2019-12-24 17:20:12 +0200

    add user page mat-list

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -4,12 +4,14 @@ import { UsersRoutingModule } from './users-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { UsersPageComponent } from './pages/users-page/users-page.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule,
-    SharedModule
+    SharedModule,
+    MatListModule
   ],
   declarations: [UsersPageComponent, UserPageComponent],
   exports: []
